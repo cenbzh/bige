3649e8229500ded82e8bd39844798644856f397c
cenbzh <cenbzh@gmail.com>
2015-12-16 23:35:23 +0800

    add the bige_define.h file

---
+++ bige_define.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: bige_define.h
+	> Author: Binzhong Cen
+	> Mail: cenbzh@gmail.com
+	> Created Time: 2015年12月16日 星期三 23时31分43秒
+ ************************************************************************/
+
+#ifndef _BIGE_DEFINE_H
+#define _BIGE_DEFINE_H
+#define MAXPOP 300
+#define MAXVAR 500
+#define MAXFUNC 100
+#endif
