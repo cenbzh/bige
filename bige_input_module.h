fc8a465ff9f3a05fb3f3a60300698904eadb4346
cenbzh <cenbzh@gmail.com>
2015-12-17 07:43:13 +0800

    input and output header

---
+++ bige_input_module.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: bige_input_module.h
+	> Author: Binzhong Cen
+	> Mail: cenbzh@gmail.com
+	> Created Time: 2015年12月17日 星期四 07时34分03秒
+ ************************************************************************/
+
+#ifndef _BIGE_INPUT_MODULE_H
+#define _BIGE_INPUT_MODULE_H
+
+void bige_input(FILE* fp);/*输入函数*/
+
+#endif
---
+++ bige_output_module.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: bige_output_module.h
+	> Author: Binzhong Cen
+	> Mail: cenbzh@gmail.com
+	> Created Time: 2015年12月17日 星期四 07时36分04秒
+ ************************************************************************/
+
+#ifndef _BIGE_OUTPUT_MODULE_H
+#define _BIGE_OUTPUT_MODULE_H
+
+void bige_output(Population* pop,FILE* fp,int type);/*输出函数*/
+
+#endif
